336045080ab62866fae6380159e9b6f341547fa4
Ender Mac <ender.spam.guru@gmail.com>
2023-05-25 13:46:29 +0300

    Vähä kamaa

--- reader.py
+++ reader.py
@@ -2,11 +2,29 @@ def reader(file):
     print("")
     print(file)
     print("")
-    with open(file, 'r') as f:
-        for line in f:
-            if file == "pirkanmaa.txt":
-                pass
-            else:
+    with open(file, 'r') as fp:
+        f = fp.readlines()
+        ans = []
+        if file == "pirkanmaa.txt":
+            for i in range(0,45):
+                data = f[i].split(" ")
+                paikka = data[0].replace("\n", "")
+                aakkoset = "AAA-ÖÖÖ"
+                if len(data) > 1:
+                    aakkoset = ''.join(data[1:]).replace("\n","")
+                ans.append((paikka, aakkoset))
+            for i in range(45,90):
+                data = f[i].split(" ")
+                aika = ""
+                j = 0
+                while ".00" not in aika and ".30" not in aika:
+                    aika += data[j] + " "
+                    print(aika)
+                    j += 1
+                sijainti = ' '.join(data[j:])
+                ans[i-45] = (ans[i-45][0], ans[i-45][1], aika, sijainti)
+        else:
+            for line in f:
                 data = line.split(" ")
                 paikka = data[0]
                 aakkoset = data[1]
@@ -23,8 +41,9 @@ def reader(file):
                 aika = ' '.join(data[i+2:i+7])
                 sijainti = ' '.join(data[i+7:])
 
-                print(
-                    f"Paikka: {paikka}  aakkoset: {aakkoset} aika: {aika} sijainti: {sijainti}")
+                ans.append((paikka, aakkoset, aika, sijainti))
+            
+        print(ans)
 
 
 # reader("hämee.txt")
