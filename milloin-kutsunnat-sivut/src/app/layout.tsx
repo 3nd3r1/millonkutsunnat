917a35fac994d4f6f7422dc5f6df706528f27079
Ender Mac <ender.spam.guru@gmail.com>
2023-05-31 20:38:29 +0300

    Hyvää matkaa koodi

--- milloin-kutsunnat-sivut/src/app/layout.tsx
+++ milloin-kutsunnat-sivut/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "./globals.scss";
 import { Oswald } from "next/font/google";
 
@@ -15,6 +16,20 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="fi">
+			<Head>
+				<title>{metadata.title}</title>
+				<meta name="description" content={metadata.description} />
+				<meta name="theme-color" content="#000000" />
+				<meta name="og:title" content={metadata.title} />
+				<meta name="og:description" content={metadata.description} />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+				<link sizes="any" rel="icon" href="/favicon.ico" />
+				<link sizes="any" rel="shortcut icon" href="/favicon.ico" />
+				<link sizes="any" rel="apple-touch-icon" href="/favicon.ico" />
+			</Head>
 			<body className={font.className}>{children}</body>
 		</html>
 	);
