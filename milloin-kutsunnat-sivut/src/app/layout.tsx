3a65096049a3b6d50bea04af921786af96e79db5
3nd3r1 <ender.spam.guru@gmail.com>
2023-06-27 13:13:29 +0300

    Korjataan metadatat ja adsense

--- milloin-kutsunnat-sivut/src/app/layout.tsx
+++ milloin-kutsunnat-sivut/src/app/layout.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import Script from "next/script";
-=======
-import Head from "next/head";
->>>>>>> bbc4bb8c8ad7a034f6d5b58ba2680db582869411
 import "./globals.scss";
 import { Oswald } from "next/font/google";
 
@@ -11,6 +7,16 @@ const font = Oswald({ subsets: ["latin"] });
 export const metadata = {
 	title: "Milloin Kutsunnat?",
 	description: "Selvitä milloin kutsuntasi ovat.",
+	themeColor: "#000000",
+	openGraph: {
+		title: "Milloin Kutsunnat?",
+		description: "Selvitä milloin kutsuntasi ovat.",
+	},
+	icons: {
+		icon: "/favicon.ico",
+		shortcut: "/favicon.ico",
+		apple: "/favicon.ico",
+	},
 };
 
 export default function RootLayout({
@@ -20,28 +26,17 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="fi">
-<<<<<<< HEAD
 			<Script
+				id="Adsense-id"
+				data-ad-client="ca-pub-8759085629417280"
 				async
+				strategy="afterInteractive"
+				onError={(e) => {
+					console.error("Script failed to load", e);
+				}}
 				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8759085629417280"
 				crossOrigin="anonymous"
 			></Script>
-=======
-			<Head>
-				<title>{metadata.title}</title>
-				<meta name="description" content={metadata.description} />
-				<meta name="theme-color" content="#000000" />
-				<meta name="og:title" content={metadata.title} />
-				<meta name="og:description" content={metadata.description} />
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1"
-				/>
-				<link sizes="any" rel="icon" href="/favicon.ico" />
-				<link sizes="any" rel="shortcut icon" href="/favicon.ico" />
-				<link sizes="any" rel="apple-touch-icon" href="/favicon.ico" />
-			</Head>
->>>>>>> bbc4bb8c8ad7a034f6d5b58ba2680db582869411
 			<body className={font.className}>{children}</body>
 		</html>
 	);
