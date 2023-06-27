e9739a480ef03670ddcf63307d891663e3faabaa
3nd3r1 <ender.spam.guru@gmail.com>
2023-06-27 13:09:23 +0300

    Sitemap and Adsense

--- milloin-kutsunnat-sivut/src/app/layout.tsx
+++ milloin-kutsunnat-sivut/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.scss";
 import { Oswald } from "next/font/google";
 
@@ -15,6 +16,11 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="fi">
+			<Script
+				async
+				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8759085629417280"
+				crossOrigin="anonymous"
+			></Script>
 			<body className={font.className}>{children}</body>
 		</html>
 	);
