15a1470e4c1f21704b928808350abde436cd23aa
3nd3r1 <ender.spam.guru@gmail.com>
2023-12-14 13:54:09 +0200

    Muutamia parannuksia

--- milloin-kutsunnat-sivut/src/app/layout.tsx
+++ milloin-kutsunnat-sivut/src/app/layout.tsx
@@ -1,21 +1,53 @@
 import Script from "next/script";
 import "./globals.scss";
 import { Oswald } from "next/font/google";
+import Link from "next/link";
+import { Metadata } from "next";
 
 const font = Oswald({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
 	title: "Milloin Kutsunnat?",
-	description: "Selvitä milloin kutsuntasi ovat.",
-	themeColor: "#000000",
+	description:
+		"Selvitä milloin intti kutsuntasi ovat, ja valmistaudu armeijan palvelukseen.",
+	keywords: [
+		"armeija",
+		"kutsunnat",
+		"intti",
+		"palvelus",
+		"aamuja",
+		"kutsuntapäivä",
+	],
+	category: "army",
+	creator: "Vebbi",
+	colorScheme: "light",
+	metadataBase: new URL("https://millonkutsunnat.fi/"),
+	alternates: {
+		canonical: "/",
+	},
 	openGraph: {
 		title: "Milloin Kutsunnat?",
-		description: "Selvitä milloin kutsuntasi ovat.",
+		description:
+			"Selvitä milloin intti kutsuntasi ovat, ja valmistaudu armeijan palvelukseen.",
+		url: "https://millonkutsunnat.fi/",
+		siteName: "Milloin Kutsunnat?",
+		images: ["/images/kutsunnat-banner.jpg"],
+		locale: "fi_FI",
+		type: "website",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "Milloin Kutsunnat?",
+		description:
+			"Selvitä milloin intti kutsuntasi ovat, ja valmistaudu armeijan palvelukseen.",
+		creator: "@vebbifi",
+		creatorId: "1671053312160923651",
+		siteId: "1671053312160923651",
+		images: ["/images/kutsunnat-banner.jpg"],
 	},
 	icons: {
-		icon: "/favicon.ico",
+		icon: [{ url: "/favicon.ico" }],
 		shortcut: "/favicon.ico",
-		apple: "/favicon.ico",
 	},
 };
 
@@ -30,14 +62,49 @@ export default function RootLayout({
 				id="Adsense-id"
 				data-ad-client="ca-pub-8759085629417280"
 				async
-				strategy="afterInteractive"
-				onError={(e) => {
-					console.error("Script failed to load", e);
-				}}
 				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8759085629417280"
 				crossOrigin="anonymous"
 			></Script>
-			<body className={font.className}>{children}</body>
+			<Script type="application/ld+json" id="schema-org">
+				{`{
+					"@context": "http://schema.org",
+					"@graph": [
+						{
+							"@type": "WebSite",
+							"@id": "https://millonkutsunnat.fi/",
+							"name": "Milloin Kutsunnat",
+							"description": "Selvitä milloin intti kutsuntasi ovat, ja valmistaudu armeijan palvelukseen.",
+							"url": "https://millonkutsunnat.fi/",
+							"logo": "https://millonkutsunnat.fi/images/kutsunnat-banner.jpg",
+							"inLanguage": "fi",
+							"datePublished": "2023-06-27T12:00:00+03:00",
+							"thumbnailUrl": "https://millonkutsunnat.fi/images/kutsunnat-banner.jpg",
+							"sameAs": [
+								"https://www.instagram.com/vebbi.fi/",
+								"https://www.linkedin.com/company/vebbi/",
+								"https://www.tiktok.com/@vebbifi",
+								"https://www.facebook.com/vebbifi"
+							]
+						}
+					]
+				}`}
+			</Script>
+			<body className={font.className}>
+				{children}
+
+				<footer className="w-full absolute bottom-0">
+					<p className="absolute right-4 sm:bottom-2">
+						Powered by{" "}
+						<Link
+							className="font-bold text-green-500"
+							href="https://vebbi.fi"
+							target="_blank"
+						>
+							Vebbi
+						</Link>
+					</p>
+				</footer>
+			</body>
 		</html>
 	);
 }
